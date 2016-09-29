6afbc56e20e05e50c5061db96bd82a5333f4f2c2
smritibhati <smriti.bhati@gmail.com>
2016-09-29 18:53:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,36 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne = {
+    title: 'Article one',
+    heading: 'Welcome to sem 1',
+    content: 'This is going to talk about your first semester. All the best with it.'
+};
+function createTemplate(data)
+{
+    
+
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title> ${data.title} </title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <div>Welcome to sem 1</div>
+            <h>
+                ${data.heading}
+            </h>
+            <p> 
+            ${data.content}
+            </p>
+        </body>
+    </html> 
+    ` ; 
+ return htmlTemplate;
+}
+    
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -17,8 +47,17 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/articleone', function (req, res) {
+  res.send(createTemplate(articleone));
+});
+app.get('/articletwo', function (req, res) {
+  res.send(createTemplate(articletwo));
+});
+app.get('/articlethree', function (req, res) {
+   res.send(createTemplate(articlethree));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log('IMAD course app listening on port ${port}!');
 });
