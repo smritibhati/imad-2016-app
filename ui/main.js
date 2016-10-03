ea7c7cc83a169a0778f1ad9ad13f4d6ce6d83eee
smritibhati <smriti.bhati@gmail.com>
2016-10-03 20:58:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,6 +32,7 @@
       var list= '';
       for (var i=0;i<names.length;i++)
           list += '<li>' + names[i] + '</li>';
+    console.log(list);
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
     };
