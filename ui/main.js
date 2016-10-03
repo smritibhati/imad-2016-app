2956bf3b282f3a1c075e389c9ca55d5bfe437902
smritibhati <smriti.bhati@gmail.com>
2016-10-03 19:33:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,18 @@
 var button= document.getElementById('counter');
 var counter=0;
-button.onClick= function() {
+button.onClick = function() {
 
-    
-    counter=counter +1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange= function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if( request.status === 200 ){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open( 'GET','http://smritibhati.imad.hasura-app.io/',true);
+    request.send(null);
 };
 
