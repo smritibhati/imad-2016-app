f404658e975499f3486bee2d4f47d5b8cac7d877
smritibhati <smriti.bhati@gmail.com>
2016-10-03 21:06:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,7 @@
     var submit= document.getElementById('submit_btn');
     
     submit.onclick = function() {
-      alert("clicked"); 
-      var names=['name1','name2','name3','name4'];
+       var names=['name1','name2','name3','name4'];
       var list= '';
       for (var i=0;i<names.length;i++)
           list += '<li>' + names[i] + '</li>';
