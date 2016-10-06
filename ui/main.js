7e69c30bf9cc6b6fa506a8da5ce222f72b7359aa
smritibhati <smriti.bhati@gmail.com>
2016-10-06 19:38:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,17 +21,19 @@
      request.open('GET','http://smritibhati.imad.hasura-app.io/counter',true);
      request.send(null);
     };
-    
+    //submit name
     var nameInput = document.getElementById('name');
     var name= nameInput.value;
     var submit= document.getElementById('submit_btn');
     
     submit.onclick = function() {
-       var names=['name1','name2','name3','name4'];
+       //make a request and send a name
+       //capture a list of names and display
+      var names=['name1','name2','name3'];
       var list= '';
       for (var i=0;i<names.length;i++)
           list += '<li>' + names[i] + '</li>';
-    console.log(list);
+    
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
     };
