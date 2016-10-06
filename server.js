16882b2fcc2f3ce07f4b3a8e4f2e77b6b55f78b1
smritibhati <smriti.bhati@gmail.com>
2016-10-06 19:48:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,8 +106,8 @@ var articleName= req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
-  var name=req.params.name;
+app.get('/submit-name/', function (req, res) {
+  var name=req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
 });
