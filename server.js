a1375a2b4d439d4b6cac2144188442f55081b95d
smritibhati <smriti.bhati@gmail.com>
2016-10-06 19:45:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,6 +105,14 @@ app.get('/:articleName', function (req, res) {
 var articleName= req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+  var name=req.params.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
